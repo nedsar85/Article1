dfe0a40133e3e6ef9bc0ae7f1b9a36962dc7f619
Nestor Sanchez <75551508+nedsar85@users.noreply.github.com>
2023-06-07 12:46:01 +0100

    Update CODE.ino

--- S1 - Arduino Code/CODE.ino
+++ S1 - Arduino Code/CODE.ino
@@ -190,16 +190,31 @@ void loop()
 // ######
 // STM allows use of library under the following licenses
 // • STMDUINO Library license: BSD 3-Clause License
-// Copyright (c) 2017, STMicroelectronics All rights reserved.
 
-// • LGPLv2.1
-// Copyright (C) 1991, 1999 Free Software Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA 
-// Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+//Copyright (c) 2016, STMicroelectronics
+//All rights reserved.
 
-// • Ultimate Liberty License
-// This software component is licensed by ST under Ultimate Liberty license SLA0044, the "License"; You may not use this file except in compliance with the License. You may obtain a copy of the License at: http://www.st.com/SLA0044
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
 
-// • Apache License
-// Version 2.0, January 2004  http://www.apache.org/licenses/
+//1. Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
 
-// Full description of terms and conditions: // https://github.com/stm32duino/Arduino_Core_STM32/blob/main/License.md#Ultimate-Liberty-License
+//2. Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+//3. Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+ //  this software without specific prior written permission.
+
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
